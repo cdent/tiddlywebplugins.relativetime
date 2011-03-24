3c3e896d6ff558846436f0955a95f5f93a9456d5
Chris Dent <chris.dent@gmail.com>
2011-03-24 15:29:08 +0000

    release 0.4 twp.relativetime

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ AUTHOR = 'Chris Dent'
 AUTHOR_EMAIL = 'cdent@peermore.com'
 NAME = 'tiddlywebplugins.relativetime'
 DESCRIPTION = 'Use relative time expressions in select filters'
-VERSION = '0.3'
+VERSION = '0.4'
 
 
 import os
